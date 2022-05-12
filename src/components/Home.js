a8b876c6529e055e871b13b392bf64af15d0afaf
joker-jonesy <spashmcgrath94@yahoo.com>
2022-05-12 10:45:57 -0400

    Home Commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,15 +3,56 @@ import React from "react";
 class Home extends React.Component {
     constructor() {
         super();
+        this.state={
+            form:{
+                name:"",
+                title:"",
+                brand:""
+            },
+            complete:""
+        }
+        this.valueChange=this.valueChange.bind(this);
+        this.submit=this.submit.bind(this);
+    }
+
+    valueChange(event){
+        let newForm = this.state.form;
+        const name =event.target.name;
+
+        newForm ={
+            ...newForm,
+            [name]:event.target.value
+        }
+
+        this.setState({
+            form:newForm
+        })
+    }
+
+    submit(){
+        console.log(this.state.form);
+
+        //error handling
+        this.setState({
+            form:{
+              name:"",
+              title:"",
+              brand:""
+            },
+            complete:"This is completed"
+        })
+
+
     }
 
     render() {
         return (
             <div>
-                <input type="text" placeholder={"Name..."}/>
-                <input type="text" placeholder={"Title..."}/>
-                <input type="text" placeholder={"Name..."}/>
-                <button>Submit</button>
+                <input name={"name"} type="text" placeholder={"Name..."} value={this.state.form.name} onChange={this.valueChange}/>
+                <input name={"title"} type="text" placeholder={"Title..."} value={this.state.form.title} onChange={this.valueChange}/>
+                <input name={"brand"} type="text" placeholder={"brand..."} value={this.state.form.brand} onChange={this.valueChange}/>
+                <button onClick={this.submit}>Submit</button>
+                <h1>{this.state.complete}</h1>
             </div>
         )
     }
